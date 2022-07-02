410a2d29d637990544bb0f328790f4f8ba8cebd0
brian <brianochieng9725@gmail.com>
2022-07-01 19:34:32 -0700

    This program will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,66 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- * main - Program Entry point
- *
- * Description: A program to determine wether an int is +ve, -ve or 0
- *
- * Return: 0 indicates success
- */
 
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n, last;
+	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 
-	last = n % 10;
+	x = n % 10;
+	printf("Last digit of %d is %d ", n, x);
+	if (x > 5)
+	{
+		printf("and is greater than 5");
+	}
+	if (x == 0)
+	{
+		printf("and is 0");
+	}
 
-	if (last >= 6)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else if (last < 6 && last != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	else
-		printf("Last digit of %d is 0 and is 0\n", n);
+	printf("\n");
+
 	return (0);
 }
-
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-
