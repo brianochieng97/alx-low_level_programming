b92e53ea1e0e104eefa7940974786c07671050a2
brian <brianochieng9725@gmail.com>
2022-07-01 18:51:21 -0700

    Hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,21 +11,19 @@ int main(void)
 
 	for (num = 0; num <= 98; num++)
 	{
-
-
-	for (num = 0; num <= 98; num++)
-	{
-		putchar((num / 10) + '0');
-		putchar((num % 10) + '0');
-		putchar(' ');
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (num == 98 && i == 99)
-			continue;
-		putchar(',');
-		putchar(' ');
+		for (i = num + 1; i <= 99; i++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			if (num == 98 && i == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
